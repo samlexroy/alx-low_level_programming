dbc972fc6506e83bb419ec0fa2f7636eb2cc9218
samlexroy <roi.educonsult@gmail.com>
2023-02-20 09:32:02 -0800

    sometimes suffer from insomnia

---
+++ 2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - maint function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
