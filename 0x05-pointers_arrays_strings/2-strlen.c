b337f168e5f6d085473e89190c9e82abb3c03ae2
samlexroy <roi.educonsult@gmail.com>
2023-02-27 10:54:08 -0800

    This report, by its very length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - function to get the lenght of a string
+ * @s: string pointer to pass to this function
+ * Return: returns the length of the strings
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
